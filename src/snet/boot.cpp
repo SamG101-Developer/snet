3b5892bd69bbd82d1026c0ff5016a27f1dcb75a9
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-16 05:50:46 +0000

    Fix serializable types

--- src/snet/boot.cpp
+++ src/snet/boot.cpp
@@ -17,18 +17,27 @@ auto snet::boot::boot_serex() -> void {
     // Register serialization polymorphic types.
     serex::register_polymorphic_type<comm_stack::RawRequest>("snet.comm_stack.RawRequest");
     serex::register_polymorphic_type<comm_stack::EncryptedRequest>("snet.comm_stack.EncryptedRequest");
-    serex::register_polymorphic_type<comm_stack::LayerD_BootstrapRequest>("snet.comm_stack.layers.LayerD_BootstrapRequest");
-    serex::register_polymorphic_type<comm_stack::LayerD_BootstrapResponse>("snet.comm_stack.layers.LayerD_BootstrapResponse");
+
     serex::register_polymorphic_type<comm_stack::Layer4_ConnectionRequest>("snet.comm_stack.layers.Layer4_ConnectionRequest");
     serex::register_polymorphic_type<comm_stack::Layer4_ConnectionAccept>("snet.comm_stack.layers.Layer4_ConnectionAccept");
     serex::register_polymorphic_type<comm_stack::Layer4_ConnectionAck>("snet.comm_stack.layers.Layer4_ConnectionAck");
     serex::register_polymorphic_type<comm_stack::Layer4_ConnectionClose>("snet.comm_stack.layers.Layer4_ConnectionClose");
+
+    serex::register_polymorphic_type<comm_stack::Layer3_PingRequest>("snet.comm_stack.layers.Layer3_PingRequest");
+    serex::register_polymorphic_type<comm_stack::Layer3_PongRequest>("snet.comm_stack.layers.Layer3_PongRequest");
+    serex::register_polymorphic_type<comm_stack::Layer3_FindNodeRequest>("snet.comm_stack.layers.Layer3_FindNodeRequest");
+    serex::register_polymorphic_type<comm_stack::Layer3_FindNodeResponse>("snet.comm_stack.layers.Layer3_FindNodeResponse");
+
+    serex::register_polymorphic_type<comm_stack::LayerD_BootstrapRequest>("snet.comm_stack.layers.LayerD_BootstrapRequest");
+    serex::register_polymorphic_type<comm_stack::LayerD_BootstrapResponse>("snet.comm_stack.layers.LayerD_BootstrapResponse");
+
     serex::register_polymorphic_type<comm_stack::Layer2_RouteExtensionRequest>("snet.comm_stack.layers.Layer2_RouteExtensionRequest");
     serex::register_polymorphic_type<comm_stack::Layer2_TunnelJoinRequest>("snet.comm_stack.layers.Layer2_TunnelJoinRequest");
     serex::register_polymorphic_type<comm_stack::Layer2_TunnelJoinAccept>("snet.comm_stack.layers.Layer2_TunnelJoinAccept");
     serex::register_polymorphic_type<comm_stack::Layer2_TunnelJoinReject>("snet.comm_stack.layers.Layer2_TunnelJoinReject");
     serex::register_polymorphic_type<comm_stack::Layer2_TunnelDataForward>("snet.comm_stack.layers.Layer2_TunnelDataForward");
     serex::register_polymorphic_type<comm_stack::Layer2_TunnelDataBackward>("snet.comm_stack.layers.Layer2_TunnelDataBackward");
+
     serex::register_polymorphic_type<comm_stack::Layer1_ApplicationLayerRequest>("snet.comm_stack.layers.Layer1_ApplicationLayerRequest");
     serex::register_polymorphic_type<comm_stack::Layer1_ApplicationLayerResponse>("snet.comm_stack.layers.Layer1_ApplicationLayerResponse");
 
