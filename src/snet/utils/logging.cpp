e5132c87305e8b90b108699d96aab4dd915dd568
SamG101-Developer <samuelgardner101@gmail.com>
2025-12-08 18:42:28 +0000

    Tidy

--- src/snet/utils/logging.cpp
+++ src/snet/utils/logging.cpp
@@ -5,8 +5,8 @@ import std;
 
 export namespace snet::utils {
     auto create_logger(std::string const &name) -> std::shared_ptr<spdlog::logger> {
+        const auto postfix_name = std::to_string(reinterpret_cast<std::ptrdiff_t>(&name));
         auto sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        auto postfix_name = std::to_string(reinterpret_cast<std::ptrdiff_t>(&name));
         auto logger = std::make_shared<spdlog::logger>(name + "-" + postfix_name, sink);
         logger->set_level(spdlog::level::level_enum::debug);
         spdlog::register_logger(logger);
